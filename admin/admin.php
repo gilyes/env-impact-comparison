d4e204b16b2cfc0414e09eadef095ec3cd2ea87f
gilyes <gilyes@users.noreply.github.com>
2019-01-22 19:28:11 -0700

    Add settings page

--- admin/admin.php
+++ admin/admin.php
@@ -30,7 +30,110 @@ class EnvImpactComparison_Admin
 
     public function register_admin_page()
     {
-
+        add_settings_section("env_impact_comparison_settings", "Settings", null, "env_impact_comparison");
+        add_settings_field("electric-vehicles-csv", "Upload EV CSV", array($this, "electric_vehicles_upload"), "env_impact_comparison", "env_impact_comparison_settings");
+        add_settings_field("ics-vehicles-csv", "Upload ICS CSV", array($this, "ice_vehicles_upload"), "env_impact_comparison", "env_impact_comparison_settings");
+        register_setting("env_impact_comparison_settings", "electric-vehicles-csv", array($this, "handle_electric_vehicles_upload"));
+        register_setting("env_impact_comparison_settings", "ice-vehicles-csv", array($this, "handle_ice_vehicles_upload"));
     }
 
+    public function admin_menu_item()
+    {
+        add_submenu_page("options-general.php", "Environmental Impact Comparison", "Environmental Impact Comparison",
+            "manage_options", $this->plugin_name, array($this, 'admin_display'));
+    }
+
+    public function handle_electric_vehicles_upload($option)
+    {
+        if (!empty($_FILES["electric-vehicles-csv"]["tmp_name"])) {
+            $content = $this->read_uploaded_file($_FILES["electric-vehicles-csv"]);
+            if (isset($content)) {
+                // TODO: save to db
+            }
+        }
+
+        return $option;
+    }
+
+    public function handle_electric_ice_upload($option)
+    {
+        if (!empty($_FILES["electric-ice-csv"]["tmp_name"])) {
+            $content = $this->read_uploaded_file($_FILES["ice-vehicles-csv"]);
+            if (isset($content)) {
+                // TODO: save to db
+            }
+        }
+
+        return $option;
+    }
+
+    public function admin_display()
+    {
+        echo '
+      	<div class="wrap">
+        <h1>Environmental Impact Comparison - Configuration</h1>
+		<form method="post" action="options.php" enctype="multipart/form-data">';
+
+        settings_fields("env_impact_comparison_settings");
+        do_settings_sections("env_impact_comparison");
+        submit_button();
+
+        echo '</form></div>';
+    }
+
+    public function electric_vehicles_upload()
+    {
+        echo '<input type="file" name="electric-vehicles-csv" />';
+        echo get_option('electric-vehicles-csv');
+    }
+
+    public function ice_vehicles_upload()
+    {
+        echo '<input type="file" name="ice-vehicles-csv" />';
+        echo get_option('ice-vehicles-csv');
+    }
+
+    public function add_settings_link($links)
+    {
+        $settings_link = "<a href='" . admin_url("options-general.php?page=$this->plugin_name") . "'>" . __('Settings') . '</a>';
+        array_push($links, $settings_link);
+        return $links;
+    }
+
+    private function read_uploaded_file($uploadInfo)
+    {
+        $fileName = $uploadInfo["tmp_name"];
+        if (!is_uploaded_file($fileName)) {
+            return null;
+        }
+
+        try {
+            // Undefined | Multiple Files | $_FILES Corruption Attack
+            // If this request falls under any of them, treat it invalid.
+            if (!isset($uploadInfo['error']) ||
+                is_array($uploadInfo['error'])) {
+                throw new RuntimeException('Invalid parameters.');
+            }
+
+            switch ($uploadInfo['error']) {
+                case UPLOAD_ERR_OK:
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    throw new RuntimeException('No file sent.');
+                case UPLOAD_ERR_INI_SIZE:
+                case UPLOAD_ERR_FORM_SIZE:
+                    throw new RuntimeException('Exceeded filesize limit.');
+                default:
+                    throw new RuntimeException('Unknown errors.');
+            }
+
+        } catch (RuntimeException $e) {
+            error_log($e);
+            return null;
+        }
+
+        $contents = file_get_contents($fileName);
+
+        return $contents;
+    }
 }
--- includes/env-impact-comparison.php
+++ includes/env-impact-comparison.php
@@ -55,7 +55,10 @@ class EnvImpactComparison
 
         $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
         $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
-        $this->loader->add_action('admin_menu', $plugin_admin, 'register_admin_page');
+        $this->loader->add_action('admin_init', $plugin_admin, 'register_admin_page');
+		$this->loader->add_action('admin_menu', $plugin_admin, 'admin_menu_item');
+
+        $this->loader->add_filter("plugin_action_links_$this->plugin_name/main.php", $plugin_admin, 'add_settings_link');
     }
 
     private function define_public_hooks()
