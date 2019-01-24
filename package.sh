6f8f531e434059e0160c9ca3a29e0ea7db15ad0f
gilyes <gilyes@users.noreply.github.com>
2019-01-24 00:07:38 -0700

    Fix packaging script to have a root folder

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-7z a -tzip releases/$1/env-impact-comparison.zip ./ -xr\!node_modules -xr\!.git -x\!releases
+7z a -tzip releases/$1/env-impact-comparison.zip ../env-impact-comparison -xr\!node_modules -xr\!.git -x\!releases
